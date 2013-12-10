fad4b7442708d75eefdab57a680ab8ee82ae3159
ianjuma <ian.juma@yahoo.com>
2013-12-10 03:56:35 +0300

    runner init script

---
+++ runner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: runner.sh
+# 
+#         USAGE: ./runner.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 12/01/2013 08:22:48 PM EAT
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+echo "am here, doing nothing"
+
