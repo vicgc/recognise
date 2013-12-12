992991fe0a6a84c75d5b6b94d6f5252825b3f349
ianjuma <ian.juma@yahoo.com>
2013-12-12 12:50:22 +0300

    rpm to deb change

--- get-cl.sh
+++ get-cl.sh
@@ -24,3 +24,8 @@ wget -r -c --tries=2 http://registrationcenter.intel.com/irc_nas/3555/intel_sdk_
 
 tar zxvf intel_sdk_for_ocl_applications_xe_2013_r2_sdk_3.1.1.11385_x64.tgz
 
+fakeroot alien --to-deb opencl-1.2-intel-devel-3.1.1.11385-1.x86_64.rpm
+fakeroot alien --to-deb opencl-1.2-intel-mic-3.1.1.11385-1.x86_64.rpm
+fakeroot alien --to-deb opencl-1.2-base-3.1.1.11385-1.x86_64.rpm
+fakeroot alien --to-deb opencl-1.2-devel-3.1.1.11385-1.x86_64.rpm
+fakeroot alien --to-deb opencl-1.2-intel-cpu-3.1.1.11385-1.x86_64.rpm
