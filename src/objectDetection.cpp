2a786f3cf0ae7deb987c95fe499fb2e3dcd20ebc
ianjuma <ian.juma@yahoo.com>
2013-12-18 19:38:59 +0300

    obejct tracker + detection update

--- src/objectDetection.cpp
+++ src/objectDetection.cpp
@@ -20,8 +20,7 @@ string window_name = "Capture - Face detection";
 RNG rng(12345);
 
 /** @function main */
-int main( int argc, const char** argv )
-{
+int main( int argc, const char** argv ) {
  CvCapture* capture;
  Mat frame;
 
@@ -31,17 +30,16 @@ int main( int argc, const char** argv )
 
  //-- 2. Read the video stream
  capture = cvCaptureFromCAM( -1 );
- if( capture )
- {
+ if( capture ) {
    while( true )
    {
  frame = cvQueryFrame( capture );
 
  //-- 3. Apply the classifier to the frame
-     if( !frame.empty() )
-     { detectAndDisplay( frame ); }
-     else
-     { printf(" --(!) No captured frame -- Break!"); break; }
+     if( !frame.empty() ) { detectAndDisplay( frame ); }
+     else { 
+      printf(" --(!) No captured frame -- Break!"); break; 
+     }
 
      int c = waitKey(10);
      if( (char)c == 'c' ) { break; }
@@ -51,8 +49,7 @@ int main( int argc, const char** argv )
 }
 
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat frame )
-{
+void detectAndDisplay( Mat frame ) {
 std::vector<Rect> faces;
 Mat frame_gray;
 
@@ -62,8 +59,7 @@ equalizeHist( frame_gray, frame_gray );
 //-- Detect faces
 face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 
-for( size_t i = 0; i < faces.size(); i++ )
-{
+for( size_t i = 0; i < faces.size(); i++ ) {
   Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
   ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
 
@@ -73,8 +69,7 @@ for( size_t i = 0; i < faces.size(); i++ )
   //-- In each face, detect eyes
   eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 
-  for( size_t j = 0; j < eyes.size(); j++ )
-   {
+  for( size_t j = 0; j < eyes.size(); j++ ) {
      Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
      int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
      circle( frame, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
