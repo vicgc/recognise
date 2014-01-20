3470b1826cbad26d1ce656692565276b0a85564b
ianjuma <ian.juma@yahoo.com>
2014-01-20 03:07:50 +0300

    console - utf

--- src/app/console.py
+++ src/app/console.py
@@ -4,3 +4,29 @@
 from clint.textui import colored, puts, indent
 from clint.arguments import Args
 from clint import piped_in
+
+
+if __name__ == '__main__':
+
+    puts('Test:')
+    with indent(4):
+        puts('%s Fake test 1.' % colored.green('✔'))
+        puts('%s Fake test 2.' % colored.red('✖'))
+
+    puts('')
+    puts('Greet:')
+    with indent(4):
+        puts(colored.red('Здравствуйте'))
+        puts(colored.green('你好。'))
+        puts(colored.yellow('سلام'))
+        puts(colored.magenta('안녕하세요'))
+        puts(colored.blue('नमस्ते'))
+        puts(colored.cyan('γειά σου'))
+
+    puts('')
+    puts('Arguments:')
+    with indent(4):
+        puts('%s' % colored.red(args[0]))
+
+    puts('')
+    puts('File:')
