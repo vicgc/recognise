c20aaea4c435c8b3821dbb1aa84997ef9090f042
ianjuma <ian.juma@yahoo.com>
2014-01-20 03:05:31 +0300

    indent - deps

--- src/app/console.py
+++ src/app/console.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 # Print text in all colors
 
-from clint.textui import colored, puts
+from clint.textui import colored, puts, indent
+from clint.arguments import Args
+from clint import piped_in
