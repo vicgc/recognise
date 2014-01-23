fc438d0c2313bc8bc5dfb7b931157215a1c06266
ianjuma <ian.juma@yahoo.com>
2014-01-24 01:18:28 +0300

    option args

--- src/app/recogn.py
+++ src/app/recogn.py
@@ -2,12 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from optparse import OptionParser
 
 from clint.textui import colored, puts, indent
 from clint.arguments import Args
 
 
 args = Args()
+parser = OptionParser()
+
+parser.add_option("-f", "--file", dest="filename",
+                    help="write report to FILE", metavar="FILE")
+parser.add_option("-q", "--quiet",
+                    action="store_false", dest="verbose", default=True,
+                    help="don't print status messages to stdout")
+
+(options, args) = parser.parse_args()
 
 if __name__ == '__main__':
 
