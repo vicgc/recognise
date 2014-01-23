685e19e50d2c0d5af3dfc6af806cc08c188b4e0c
ianjuma <ian.juma@yahoo.com>
2014-01-23 10:12:28 +0300

    amd tools

--- get-deps.sh
+++ get-deps.sh
@@ -21,6 +21,10 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D \
 WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D \
 INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D \
 WITH_OPENCL=ONWITH_QT=ON -D WITH_OPENGL=ON ..
+
+# with amd gpu tools
+# WITH_OPENCLAMDFFT=ON, WITH_OPENCLAMDBLAS=ON
+
 make -j2
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
