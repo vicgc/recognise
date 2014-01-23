4cbeeb83f73a739ea6695161249555529f894f29
ianjuma <ian.juma@yahoo.com>
2014-01-24 01:19:59 +0300

    parser

--- src/app/recogn.py
+++ src/app/recogn.py
@@ -9,18 +9,16 @@ from clint.arguments import Args
 
 
 args = Args()
-parser = OptionParser()
 
-parser.add_option("-f", "--file", dest="filename",
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="./console.py",
                     help="write report to FILE", metavar="FILE")
 parser.add_option("-q", "--quiet",
                     action="store_false", dest="verbose", default=True,
                     help="don't print status messages to stdout")
 
-(options, args) = parser.parse_args()
-
 if __name__ == '__main__':
-
+    (options, args) = parser.parse_args()
     puts('Tests:')
     with indent(4):
         pass
