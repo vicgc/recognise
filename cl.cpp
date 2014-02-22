a636904bbc7124c7db3a7fba3973afbca252ee7f
ianjuma <ian.juma@yahoo.com>
2014-02-23 01:47:37 +0300

    cl check gpu cores

---
+++ cl.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <CL/cl.hpp>
+#include <boost/foreach.hpp>
+
+// dpkg -S /usr/include/GL/gl.h 
+// sudo apt-get install mesa-common-dev
+
+int main(int, char**) {
+ std::vector<cl::Platform> platforms;
+ cl::Platform::get(&platforms);
+ BOOST_FOREACH(cl::Platform platform, platforms) {
+     std::cout << "Platform: " << platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
+     std::vector<cl::Device> devices;
+     platform.getDevices(CL_DEVICE_TYPE_GPU | CL_DEVICE_TYPE_CPU, &devices);
+     BOOST_FOREACH(cl::Device device, devices) {
+         std::cout << "Device: " << device.getInfo<CL_DEVICE_TYPE>();
+         std::cout << " (" << CL_DEVICE_TYPE_GPU << " means GPU, " << CL_DEVICE_TYPE_CPU << " means CPU)" << std::endl;
+     }
+ }
+}
