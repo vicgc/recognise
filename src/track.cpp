dbdf28727b1c4403d1016c60ffa4a465949ef11c
ianjuma <ian.juma@yahoo.com>
2014-02-24 16:31:30 +0300

    tracker

--- src/track.cpp
+++ src/track.cpp
@@ -1,6 +1,9 @@
 #include <cv.h>
 // #include <highgui.h>
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/highgui/highgui.hpp>
+
+// g++ track.cpp -I /usr/include/opencv `pkg-config --cflags --libs opencv`
+
 
 
 IplImage* imgTracking;
