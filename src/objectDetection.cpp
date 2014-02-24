2ca3aa7bee53d293fb29fc7c2c7ac2130e1609fe
ianjuma <ian.juma@yahoo.com>
2014-02-24 16:29:31 +0300

    object detect libs pkg-config

--- src/objectDetection.cpp
+++ src/objectDetection.cpp
@@ -1,9 +1,12 @@
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <iostream>
 #include <stdio.h>
+
+
+//g++ FaceRec.cpp `pkg-config --cflags --libs opencv`
 
 using namespace std;
 using namespace cv;
