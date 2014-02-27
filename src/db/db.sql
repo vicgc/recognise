79506eb0fcdeb3691d2c6db07038a93c0f85e5be
ianjuma <ian.juma@yahoo.com>
2014-02-28 01:03:28 +0300

    table 2

--- src/db/db.sql
+++ src/db/db.sql
@@ -1,5 +1,11 @@
 CREATE USER synod WITH PASSWORD 'root';
 
+CREATE DATABASE recognise
+  OWNER = synod
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = 25;
+
+
 CREATE TABLE class_recogn
 (
     id_no serial NOT NULL,
@@ -10,14 +16,22 @@ CREATE TABLE class_recogn
     last_time_stamp TIMESTAMP,
 
     PRIMARY KEY (id_no)
+)
 
+
+CREATE TABLE sec_recogn
+(
+    id_no serial NOT NULL,
+    first_name varchar(20) NOT NULL,
+    last_name varchar(20) NOT NULL,
+    last_time_stamp TIMESTAMP,
+    zone varchar(20) NOT NULL,
+
+    PRIMARY KEY (id_no)
 )
 
 
 GRANT ALL PRIVILEGES ON class_recogn to synod;
 
-
-CREATE DATABASE recognise
-  OWNER = synod
-  ENCODING = 'UTF8'
-  CONNECTION LIMIT = 25;
+\c recognise
+SELECT version()
