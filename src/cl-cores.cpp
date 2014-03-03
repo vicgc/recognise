2ce050a695a3a1f1f9521d2d2365c3db6c50e9ba
ianjuma <ian.juma@yahoo.com>
2014-03-03 10:48:32 +0300

    typo spacing

--- src/cl-cores.cpp
+++ src/cl-cores.cpp
@@ -2,22 +2,22 @@
 #include <CL/cl.hpp>
 #include <boost/foreach.hpp>
 
-// dpkg -S /usr/include/GL/gl.h 
+// dpkg -S /usr/include/GL/gl.h
 // sudo apt-get install mesa-common-dev
 // g++ -o clDeviceQuery -I/opt/intel/opencl-1.2-3.0.67279/include cl.cpp -lOpenCL
 
 int main(int argc, char *argv[]) {
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
-    
+
     BOOST_FOREACH(cl::Platform platform, platforms) {
         std::cout << "Platform: " << platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
         std::vector<cl::Device> devices;
         platform.getDevices(CL_DEVICE_TYPE_GPU | CL_DEVICE_TYPE_CPU, &devices);
-        
+ 
         BOOST_FOREACH(cl::Device device, devices) {
             std::cout << "Device: " << device.getInfo<CL_DEVICE_TYPE>();
-            std::cout << " (" << CL_DEVICE_TYPE_GPU << " means GPU, " 
+            std::cout << " (" << CL_DEVICE_TYPE_GPU << " means GPU, "
                 << CL_DEVICE_TYPE_CPU << " means CPU)" << std::endl;
         }
     }
