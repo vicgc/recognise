635ab89bef6b78bdce2260cfe3dcd9c4f785db52
ianjuma <ian.juma@yahoo.com>
2014-03-03 10:45:28 +0300

    cores id_info

--- src/cl-cores.cpp
+++ src/cl-cores.cpp
@@ -7,15 +7,18 @@
 // g++ -o clDeviceQuery -I/opt/intel/opencl-1.2-3.0.67279/include cl.cpp -lOpenCL
 
 int main(int, char**) {
- std::vector<cl::Platform> platforms;
- cl::Platform::get(&platforms);
- BOOST_FOREACH(cl::Platform platform, platforms) {
-     std::cout << "Platform: " << platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
-     std::vector<cl::Device> devices;
-     platform.getDevices(CL_DEVICE_TYPE_GPU | CL_DEVICE_TYPE_CPU, &devices);
-     BOOST_FOREACH(cl::Device device, devices) {
-         std::cout << "Device: " << device.getInfo<CL_DEVICE_TYPE>();
-         std::cout << " (" << CL_DEVICE_TYPE_GPU << " means GPU, " << CL_DEVICE_TYPE_CPU << " means CPU)" << std::endl;
-     }
- }
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    
+    BOOST_FOREACH(cl::Platform platform, platforms) {
+        std::cout << "Platform: " << platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
+        std::vector<cl::Device> devices;
+        platform.getDevices(CL_DEVICE_TYPE_GPU | CL_DEVICE_TYPE_CPU, &devices);
+        
+        BOOST_FOREACH(cl::Device device, devices) {
+            std::cout << "Device: " << device.getInfo<CL_DEVICE_TYPE>();
+            std::cout << " (" << CL_DEVICE_TYPE_GPU << " means GPU, " 
+                << CL_DEVICE_TYPE_CPU << " means CPU)" << std::endl;
+        }
+    }
 }
