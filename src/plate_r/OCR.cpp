1cf500b7a7e74df2d7829ee94e7a0ffa182c9315
ianjuma <ian.juma@yahoo.com>
2014-03-05 17:34:14 +0300

    ocr dir

--- src/plate_r/OCR.cpp
+++ src/plate_r/OCR.cpp
@@ -11,7 +11,8 @@
 
 #include "OCR.h"
 
-const char OCR::strCharacters[] = {'0','1','2','3','4','5','6','7','8','9','B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'};
+const char OCR::strCharacters[] = {'0','1','2','3','4','5','6','7','8','9','B', 'C', 'D', 'F', 'G'
+    , 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'};
 const int OCR::numCharacters=30;
 
 CharSegment::CharSegment(){}
