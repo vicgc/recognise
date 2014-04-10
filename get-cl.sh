c4a7e9083427cc2cb69037879fc54d9f4f6d5ddc
ianjuma <ian.juma@yahoo.com>
2014-04-11 02:18:41 +0300

    git cl

--- get-cl.sh
+++ get-cl.sh
@@ -2,7 +2,7 @@
 #          FILE: get-cl.sh
 # 
 #         USAGE: ./get-cl.sh 
-set -o nounset                              # Treat unset variables as an error
+set -o nounset
 
 
 wget -r -c --tries=2 http://registrationcenter.intel.com/irc_nas/3555/intel_sdk_for_ocl_applications_xe_2013_r2_sdk_3.1.1.11385_x64.tgz
