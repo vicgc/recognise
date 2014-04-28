538b411e9d1577036c6622bd09b4d44d12df906a
ianjuma <ian.juma@yahoo.com>
2014-04-29 02:15:01 +0300

    git commit

--- get-cl.sh
+++ get-cl.sh
@@ -11,11 +11,14 @@ tar zxvf intel_sdk_for_ocl_applications_xe_2013_r2_sdk_3.1.1.11385_x64.tgz
 
 cd intel_sdk_for_ocl_applications_xe_2013_r2_sdk_3.1.1.11385_x64
 
-fakeroot alien --to-deb opencl-1.2-intel-devel-3.1.1.11385-1.x86_64.rpm
-fakeroot alien --to-deb opencl-1.2-intel-mic-3.1.1.11385-1.x86_64.rpm
-fakeroot alien --to-deb opencl-1.2-base-3.1.1.11385-1.x86_64.rpm
-fakeroot alien --to-deb opencl-1.2-devel-3.1.1.11385-1.x86_64.rpm
-fakeroot alien --to-deb opencl-1.2-intel-cpu-3.1.1.11385-1.x86_64.rpm
+
+function fake() {
+    fakeroot alien --to-deb opencl-1.2-intel-devel-3.1.1.11385-1.x86_64.rpm
+    fakeroot alien --to-deb opencl-1.2-intel-mic-3.1.1.11385-1.x86_64.rpm
+    fakeroot alien --to-deb opencl-1.2-base-3.1.1.11385-1.x86_64.rpm
+    fakeroot alien --to-deb opencl-1.2-devel-3.1.1.11385-1.x86_64.rpm
+    fakeroot alien --to-deb opencl-1.2-intel-cpu-3.1.1.11385-1.x86_64.rpm
+}
 
 sudo dpkg -i opencl-1.2-base-3.1.1.11385-1.x86_64.deb
 sudo ln -s /usr/lib64/libOpenCL.so /usr/lib/libOpenCL.so
