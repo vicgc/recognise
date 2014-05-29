0531f1e1d3aef2f5e2d2b823ead541caf35a34d3
ianjuma <ian.juma@yahoo.com>
2014-05-29 23:30:29 +0300

    runscript

--- runner.sh
+++ runner.sh
@@ -20,3 +20,9 @@
 set -o nounset                              # Treat unset variables as an error
 
 echo "am here, doing nothing"
+
+chmod +x get-intel-ocl.sh
+chmod +x get-cl.sh
+
+./get-cl.sh
+./get-intel-ocl.sh
